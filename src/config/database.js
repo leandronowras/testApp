902997aef6c81ee9df7d79348102c2d8866a1987
Leandro Abreu <l.leandro_abreu@hotmail.com>
2020-02-25 00:43:32 -0300

    fiz a primeira migration, arrumei as pastas e o config

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
     host: 'localhost',
     username: 'postgres',
     password: 'docker',
-    database: 'gobarber',
+    database: 'gobarbe',
     define: {
         timestamps: true, // mostra o horario e a data de cada alteracao
         underscored: true, // deixa os arquivos_com_esse_formato
