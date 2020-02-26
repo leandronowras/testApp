e71f42043208ce54b171616a688ef83b70433d7d
Leandro Abreu <l.leandro_abreu@hotmail.com>
2020-02-26 00:41:47 -0300

    gerando hash com o bcrypt

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
 
 class User extends Model {
     static init(sequelize) {
@@ -6,13 +7,23 @@ class User extends Model {
             {
                 name: Sequelize.STRING,
                 email: Sequelize.STRING,
-                passworld_hash: Sequelize.STRING,
+                password: Sequelize.VIRTUAL, // virtual significa q nao vai existir no bd
+                password_hash: Sequelize.STRING,
                 provider: Sequelize.BOOLEAN,
             },
             {
                 sequelize,
             }
         );
+
+        this.addHook('beforeSave', async user => {
+            // eh um codigo q eh executado antes da informacao ir para o bd
+            if (user.password) {
+                user.password_hash = await bcryptjs.hash(user.password, 8);
+            }
+        });
+
+        return this;
     }
 }
 
