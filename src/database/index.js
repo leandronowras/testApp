971e818f72b8480897d61ceacc4b81ddf0248a93
Leandro Abreu <l.leandro_abreu@hotmail.com>
2020-04-01 13:21:46 -0300

    adicionada a funcionalidade de criacao de avatar

--- src/database/index.js
+++ src/database/index.js
@@ -1,10 +1,11 @@
 import Sequelize from 'sequelize';
 
 import User from '../app/models/User'; // importou o model
+import File from '../app/models/File';
 
 import databaseConfig from '../config/database'; // importou o necessario para entrar no bd
 
-const models = [User]; // criou um array com todos os models da aplicacao
+const models = [User, File]; // criou um array com todos os models da aplicacao
 
 class Database {
     constructor() {
@@ -14,7 +15,12 @@ class Database {
     init() {
         this.connection = new Sequelize(databaseConfig);
 
-        models.map(model => model.init(this.connection)); // inicia cada model no array
+        models
+            .map(model => model.init(this.connection)) // inicia cada model no array
+            .map(
+                model =>
+                    model.associate && model.associate(this.connection.models)
+            );
     }
 }
 
